172e7d4497949160f1f23585f7378ec750cc32ce
zqjhoney <31385145+zqjhoney@users.noreply.github.com>
2017-09-13 14:23:16 +0800

    Initial commit

---
+++ app/src/main/java/com/bwie/photoview/DescActivity.java
@@ -0,0 +1,77 @@
+package com.bwie.photoview;
+
+import android.content.Intent;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.bumptech.glide.Glide;
+
+import java.util.ArrayList;
+
+import uk.co.senab.photoview.PhotoView;
+import uk.co.senab.photoview.PhotoViewAttacher;
+
+public class DescActivity extends AppCompatActivity {
+
+    private ViewPager vp;
+
+    private ArrayList<String> list;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_desc);
+        initview();
+    }
+
+    private void initview() {
+        vp = (ViewPager) findViewById(R.id.vp);
+        Intent intent=getIntent();
+        if(intent.getExtras()!= null){
+
+            System.out.println("xxxxxxxx"+"判断传值不为空");
+            list= (ArrayList<String>) intent.getExtras().getSerializable("imgs");
+        }
+
+        vp.setAdapter(new MyAda());
+    //    System.out.println("xxxx另一个页面的"+list.toString());
+    }
+
+    class MyAda extends PagerAdapter{
+
+        @Override
+        public int getCount() {
+            return list.size();
+        }
+
+        @Override
+        public boolean isViewFromObject(View view, Object object) {
+            return view==object;
+        }
+
+        @Override
+        public Object instantiateItem(ViewGroup container, int position) {
+
+            View view=View.inflate(DescActivity.this,R.layout.item,null);
+            PhotoView pv=view.findViewById(R.id.pv);
+            Glide.with(DescActivity.this).load(list.get(position)).into(pv);
+            pv.setOnPhotoTapListener(new PhotoViewAttacher.OnPhotoTapListener() {
+                @Override
+                public void onPhotoTap(View view, float x, float y) {
+                    DescActivity.this.finish();
+                }
+            });
+
+            container.addView(view);
+            return view;
+        }
+
+        @Override
+        public void destroyItem(ViewGroup container, int position, Object object) {
+            container.removeView((View) object);
+        }
+    }
+}
---
+++ app/src/main/java/com/bwie/photoview/MainActivity.java
@@ -0,0 +1,106 @@
+package com.bwie.photoview;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.JavascriptInterface;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.io.Serializable;
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    private String url="http://mini.eastday.com/mobile/170913032156560.html";
+    private WebView wv;
+    private ArrayList<String> list;
+
+
+    @SuppressLint("JavascriptInterface")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        list = new ArrayList<>();
+
+        initview();
+        paraseImgs();
+        wv.loadUrl(url);
+        wv.addJavascriptInterface(new Clicks(),"img");
+
+
+
+    }
+
+    private void paraseImgs() {
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                //强制转换为文档类型
+
+                try {
+                    Document   doc = Jsoup.connect(url).get();
+                    //获取img标签
+                    Elements all = doc.getElementsByTag("img");
+
+                    for (Element img : all) {
+                     //   System.out.println("xxx"+img.attr("src"));
+                       list.add(img.attr("src"));
+
+                    }
+                //    System.out.println("xxxx"+list.size());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+
+
+            }
+        }).start();
+    }
+
+    private void initview() {
+        wv = (WebView) findViewById(R.id.wv);
+        wv.getSettings().setJavaScriptEnabled(true);
+
+        wv.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
+        wv.setWebViewClient(new WebViewClient(){
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+                wv.loadUrl("javascript:(function(){" +
+                        "var objs = document.getElementsByTagName(\"img\"); " +
+                        "for(var i=0;i<objs.length;i++)  " +
+                        "{"
+                        + "    objs[i].onclick=function()  " +
+                        "    {  "
+                        + "        window.img.imgsClick(this.src);  " +
+                        "    }  " +
+                        "}" +
+                        "})()");
+            }
+        });
+    }
+
+    class Clicks{
+       @JavascriptInterface
+        public void imgsClick(String imgurl){
+
+            Intent in=new Intent(MainActivity.this,DescActivity.class);
+            Bundle bundle=new Bundle();
+         //  System.out.println("xxxx跳转过去的数据"+list.toString());
+            bundle.putSerializable("imgs", list);
+            in.putExtras(bundle);
+            startActivity(in);
+        }
+    }
+}
